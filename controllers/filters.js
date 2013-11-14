b609a3bae53d128320024ac6fc72e5287ab94697
Rey Madrid <enokslaboratory@fullsail.edu>
2013-11-13 16:38:54 -0800

    Revert "Major update: Fixed routes, Created controllers, and Menu Links"
    This reverts commit 1cbfa411a9bbd3de7417eb8b92a7267424a9b490.

--- controllers/filters.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var filters = angular.module('AppCore.filters', []);
-filters.filter('AppCoreDate', function ($filter) {
-  return function (datestr, format) {
-    return $filter('date')(Date.parse(datestr) || datestr, format);
-  }
-});
